3956c36a802390fb8dbbab938b66f0bd7eee8ca4
Suresh <sureshkumar.android@gmail.com>
2022-10-20 16:30:08 +0530

    Added dimentions for different screen sizes and Login UI completed.

---
+++ app/src/main/java/com/mobile/dronesurvey/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.mobile.dronesurvey
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.mobile.dronesurvey.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
